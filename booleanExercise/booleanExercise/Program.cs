d7ee62607f1fc7876b369e0c538a8fc5159fc4d8
AnishKilaru <kilaruanishsai@gmail.com>
2019-08-21 21:57:56 -0700

    booleanExecrise

---
+++ booleanExercise/booleanExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace booleanExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age? ");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(age);
+
+            Console.WriteLine("Have you ever had a DUI? ");
+
+            bool answer = Convert.ToBoolean(Console.ReadLine());
+            
+            Console.WriteLine(answer);
+
+            Console.WriteLine("How many speeding tickets do you have");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(tickets);
+
+
+
+            bool rate = age > 15 && answer == false  && tickets <= 3;
+            Console.WriteLine("Qualified? ");
+            Console.WriteLine(rate);
+
+            Console.ReadLine();
+     
+
+        }
+    }
+}

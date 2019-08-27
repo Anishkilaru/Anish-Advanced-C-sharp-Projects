e59321435b07a20e901f53e984f47f14c058f108
AnishKilaru <kilaruanishsai@gmail.com>
2019-08-26 21:33:29 -0700

    MethodsExercise

---
+++ MethodsExercise/MethodsExercise/Methods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodsExercise
+{
+  public  class Methods
+    {
+        public int Mathoperations()
+
+        {
+          
+            Console.WriteLine("Welcome to my maths program, now type  number");
+            int uinput1 = 2;
+            int total = uinput1 * 4;
+            return total;
+
+        }
+
+
+
+
+        public int MathmethodTwo()
+        {
+            Console.WriteLine("Now it's time for method 2");
+            int total1 = 3 * 5;
+            int total2 = total1;
+            return total1;
+        }
+
+        public int MathoperationThree()
+        {
+            Console.WriteLine("Now, time for operation 3 ");
+            int uinput3 = 30;
+            int total3 = uinput3 * 2;
+            return total3;
+        }
+
+
+    }
+
+
+   
+
+}
---
+++ MethodsExercise/MethodsExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MethodsExercise
+{
+   public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to my Methods Exercise Program");
+            Methods mani = new Methods();
+
+            int userinput = Convert.ToInt32(Console.ReadLine());
+
+            Mathoperations();
+            MathmethodTwo();
+            MathoperationThree();
+        }
+    }
+}

eb6d7c4e861eeb80e3f52334fd0cd2fe450eb335
AnishKilaru <kilaruanishsai@gmail.com>
2019-08-26 22:04:14 -0700

    methodspractciepage113

---
+++ methodspratice2/methodspratice2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace methodspratice2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            methods maths = new methods();
+            int num1 = 2;
+            int num2 = 3;
+            maths.Method1(2,3);
+            maths.Method1(num1, num2);
+        }
+
+
+    }
+}
---
+++ methodspratice2/methodspratice2/methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methodspratice2
+{
+   public  class methods
+    {
+        public void Method1(int num1, int num2)
+        {
+            int total = num1 * 3;
+
+            Console.WriteLine("Second Integer is " + num2);
+            Console.ReadLine();
+
+        }
+
+    }
+}

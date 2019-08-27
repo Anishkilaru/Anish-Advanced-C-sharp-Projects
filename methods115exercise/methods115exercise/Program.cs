dea438e17152803dc19531305d9e96f3f3f4f95c
AnishKilaru <kilaruanishsai@gmail.com>
2019-08-27 07:13:51 -0700

    methods115page

---
+++ methods115exercise/methods115exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace methods115exercise
+{
+   public class Program
+    {
+      public  static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            methods maths = new methods();
+            int num1 = 4;
+            int num2 = 0;
+            
+            maths.methodOne(num1, num2 = default);
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ methods115exercise/methods115exercise/methods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methods115exercise
+{
+   public class methods
+    {
+        public  void methodOne(int num1, int num2 = default)
+        {
+            Console.WriteLine("Please pass 1st number, adn 2nd number is optional");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            
+
+            if (num2 == default)
+            {
+                int total = num1 / 4;
+                Console.WriteLine(total);
+                Console.ReadLine();
+            }
+            else
+            {
+                num2 = Convert.ToInt32(Console.ReadLine());
+                int total = (num1 + num2) / 4;
+                Console.WriteLine(total);
+                Console.ReadLine();
+            }
+
+        }
+
+        
+    }
+}

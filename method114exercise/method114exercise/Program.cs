07bcdccd3d7e22d283aab26108683e5d33319c0f
AnishKilaru <kilaruanishsai@gmail.com>
2019-08-27 06:45:42 -0700

    methodspage114

---
+++ method114exercise/method114exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace method114exercise
+{
+   public class Program
+    {
+      public  static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            methods operation = new methods();
+            int num1 = 10;
+            double num2 = 4.5;
+            string uinput;
+            uinput = Console.ReadLine();
+            int uinput2;
+            uinput2 = Convert.ToInt32(uinput);
+            operation.methodOne(num1);
+            Console.ReadLine();
+            operation.methodOne(num2);
+            Console.ReadLine();
+            operation.methodOne(uinput2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ method114exercise/method114exercise/methods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace method114exercise
+{
+   public class methods
+    {
+        public int  methodOne(int num1)
+        {
+            num1 = Convert.ToInt32(Console.ReadLine());
+            int total = num1 * 3;
+            //Console.WriteLine(total);
+            return total;
+        }
+
+        public double methodOne(double num2)
+        {
+            num2 = 4.5;
+            double total2 = num2 / 1;
+            return total2;
+        }
+
+        public int methodOne( string uinput)
+        {
+            uinput = Console.ReadLine();
+            int uinput2;
+            uinput2 = Convert.ToInt32(uinput);
+            int total3 = uinput2 + 4;
+            return Convert.ToInt32(total3);
+           
+        }
+    }
+}

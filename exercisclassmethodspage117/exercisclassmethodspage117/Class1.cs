ef293c343ccb6f523d08124d1210574da2d24674
AnishKilaru <kilaruanishsai@gmail.com>
2019-08-27 21:37:17 -0700

    exercisepage117

---
+++ exercisclassmethodspage117/exercisclassmethodspage117/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercisclassmethodspage117
+{
+    public static class Class1
+    {
+        public static int MethodOne(int num1)
+        {
+            num1 = Int32.Parse(Console.ReadLine());
+
+            int total = num1 / 2;
+            Console.WriteLine(total);
+            Console.ReadLine();
+            return total;
+         
+            
+        }
+
+        public static int MethodOne(int num1, int num2)
+        {
+            num1 = Int32.Parse(Console.ReadLine());
+            num2 = 10;
+
+            int total = num1 / num2;
+            Console.WriteLine(total);
+            Console.ReadLine();
+            return total;
+        }
+
+    }
+}
---
+++ exercisclassmethodspage117/exercisclassmethodspage117/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercisclassmethodspage117
+{
+ public   class Program
+    {
+      public  static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my class method code ");
+
+            // Class1 subclass = new Class1(); - the requirement says to declare a class to be static, but .net core throws error while instantiating a class variable.
+
+            Console.WriteLine("Please enter the number ");
+            Class1.MethodOne(10);
+            Console.ReadLine();
+            Console.WriteLine("Please enter the two numbers ");
+            Class1.MethodOne(10, 12);
+            Console.ReadLine();
+
+        }
+    }
+}

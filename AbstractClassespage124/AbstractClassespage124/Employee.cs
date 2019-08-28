2d85e236db1fc7af20d49076ba250d545ce471bf
AnishKilaru <kilaruanishsai@gmail.com>
2019-08-28 06:09:32 -0700

    Abstarctclassespage124

---
+++ AbstractClassespage124/AbstractClassespage124/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassespage124
+{
+    public class Employee : Person
+    {
+        public  override void SayName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AbstractClassespage124/AbstractClassespage124/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AbstractClassespage124
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Abstract classes program ki welcome");
+
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}
